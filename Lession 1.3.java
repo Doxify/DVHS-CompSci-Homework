43a752b26ac08b5f9a4c787258a6d31e59fcb60d
Andrei Georgescu <admin@hydrapvp.net>
2016-09-19 19:46:05 -0700

    Create Lession 1.3.java

---
+++ Lession 1.3.java
@@ -0,0 +1,46 @@
+package main;
+
+/**
+ * Created by andrei on 9/16/16.
+ */
+public class WhiteSpace {
+
+
+    public static void main(
+
+            String[]
+
+            args
+    )
+
+    {
+
+        System.out.println(
+
+                "A quote by Abraham Lincoln:"
+        );
+
+        System.out.println(
+
+
+                "Whatever you are, be a good one."
+
+        );
+
+    }
+
+
+}
+
+
+//public class WhiteSpace {
+//
+//  public static void main(String[]args){
+//
+//      System.out.println("A quote by Abraham Lincoln:");
+//
+//      System.out.println("Whatever you are, be a good one.");
+//
+//  }
+//
+//}

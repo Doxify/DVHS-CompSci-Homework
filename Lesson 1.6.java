b0067af3de81c03e7d8a0decc67a33588705c452
Andrei Georgescu <admin@hydrapvp.net>
2016-09-20 12:30:55 -0700

    Create Lesson 1.6.java

---
+++ Lesson 1.6.java
@@ -0,0 +1,24 @@
+package main;
+
+
+/**
+ * Created by andrei on 9/20/16.
+ */
+public class Name {
+
+
+    public static void main(String[]args) {
+
+    System.out.println("         AAAAAAA                GGGGGGGGGGGGGGG");
+    System.out.println("        AAAAAAAAA               GGGGGGGGGGGGGGG");
+    System.out.println("       AAAA   AAAA              GGG");
+    System.out.println("      AAAA     AAAA             GGG");
+    System.out.println("     AAAAAAAAAAAAAAA            GGG      GGGGGG");
+    System.out.println("    AAAAAA     AAAAAA           GGG      GGGGGG");
+    System.out.println("   AAAAAA       AAAAAA          GGG         GGG");
+    System.out.println("  AAAAAA         AAAAAA         GGGGGGGGGGGGGGG");
+    System.out.println(" AAAAAA           AAAAAA        GGGGGGGGGGGGGGG");
+
+    }
+
+}

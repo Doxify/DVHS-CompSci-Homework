9e4fd2cba1c424e5db6cbe611c93350d27814e4d
Andrei Georgescu <admin@hydrapvp.net>
2016-10-04 09:22:22 -0700

    Update temperature.java
    added color coding for celsius to fahrenheit only, too lazy to add it for fahrenheit to celsius

--- temperature.java
+++ temperature.java
@@ -5,18 +5,62 @@ package com.doxify.temperature;
  */
 public class Main {
 
+    // these are the colors for console
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
     public static void main(String[]args){
 
         // int celsius and fahrenheit is where you enter the starting temperature you want converted
-        int celsius = 31;
-        int fahrenheit = 80;
+        int celsius = 30;
+        int fahrenheit = 0;
 
-        // double fToC and cToF are the formulas used for conversion
+        // double fToC andr cToF are the formulas used for conversion
         double fToC = (fahrenheit - 32) * .5555;
         double cToF = (celsius * 1.8) + 32;
 
+        if(celsius + cToF  > 80){
+
+            // prints out conversion for celsius to fahrenheit
+            System.out.print("Detected celsius temperature... \nUsing celsius to fahrenheit formula...\n");
+            System.out.print(ANSI_RED+celsius + "° celsius is " + cToF + "° fahrenheit!");
+
+
+        }
+
+        else if(celsius + cToF  > 50){
+
+            // prints out conversion for celsius to fahrenheit
+            System.out.print("Detected celsius temperature... \nUsing celsius to fahrenheit formula...\n");
+            System.out.print(ANSI_YELLOW+celsius + "° celsius is " + cToF + "° fahrenheit!");
+
+        }
+
+        else if(celsius + cToF  > 20){
+
+            // prints out conversion for celsius to fahrenheit
+            System.out.print("Detected celsius temperature... \nUsing celsius to fahrenheit formula...\n");
+            System.out.print(ANSI_BLUE+celsius + "° celsius is " + cToF + "° fahrenheit!");
+
+        }
+
+        else if(celsius + cToF  > 0){
+
+            // prints out conversion for celsius to fahrenheit
+            System.out.print("Detected celsius temperature... \nUsing celsius to fahrenheit formula...\n");
+            System.out.print(ANSI_WHITE+celsius + "° celsius is " + cToF + "° fahrenheit!");
+
+        }
+
+
         // this detects if you are trying to convert both fahrenheit and celsius
-        if(celsius > 0 && fahrenheit > 0){
+        else if(celsius > 0 && fahrenheit > 0){
 
             System.out.print("Detected fahrenheit and celsius temperature... \nUsing both formulas...\n");
             System.out.print(fahrenheit + "° fahrenheit is " + fToC + "° celsius! \n");
